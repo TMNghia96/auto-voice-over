7eff284520797851e0d5e1954861397d8c4845e7
TMN69 <tranminhnghia200014@gmail.com>
2026-05-06 13:02:34 +0700

    feat: export msToTime for SrtTimelineExporter

--- src/lib/SrtOptimizer.ts
+++ src/lib/SrtOptimizer.ts
@@ -20,7 +20,7 @@ const timeToMs = (time: string): number => {
 /**
  * Convert milliseconds to SRT timestamp format
  */
-const msToTime = (ms: number): string => {
+export const msToTime = (ms: number): string => {
     const h = Math.floor(ms / 3600000);
     const m = Math.floor((ms % 3600000) / 60000);
     const s = Math.floor((ms % 60000) / 1000);
